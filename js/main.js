928833941fe3caf7edb350a9af196050bbdb4269
EngyEhab <96688558+EngyEhab@users.noreply.github.com>
2023-04-19 00:46:49 +0200

    navigation bar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var navLinks = document.querySelectorAll('.nav-item');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click' , function(){
+        navLinks.forEach(nav =>{
+            nav.classList.remove('clickedLinkStyle');
+        })
+        navLink.classList.add('clickedLinkStyle');
+    })
+});
--- system/home.php
+++ system/home.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>النظام الإلكترونى لإدارة شئون أعضاء هيئة التدريس</title>
 </head>
 <body>
     <?php
-        include('header.php')
+        include('header.php');
+       
     ?>
+
+
 </body>
 </html>
