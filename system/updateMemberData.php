730822670f59c76de70d773335240dcbf781b260
polasherif <128071824+polasherif@users.noreply.github.com>
2023-05-06 19:20:35 +0200

    test

--- system/updateMemberData.php
+++ system/updateMemberData.php
@@ -1,3 +1,171 @@
+<?php
+include "../Connections/syscon.php"; 
+$bis = mysqli_connect($hostname_bis, $username_bis, $password_bis, $database_bis);
+// $id=$_GET['id'];
+// $id="";
+// $Doctor_Code ="";
+$qualifications="";
+$date_of_birth ="";
+$hiring_date =""; 
+$Doctor_ar_Name ="";              
+$Doctor_eng_Name="";                   
+$National_id="";                   
+$Mobile="";                   
+$Academic_Mail=""; 
+$Personal_Mail=""; 
+$Notes=""; 
+$Doctor_image=""; 
+$department=""; 
+$university="";
+$faculty="";
+$doctorjob="";
+
+if (isset($_GET['id'])){
+    $id=$_GET['id'];
+
+    
+    $Select=mysqli_query($bis,"SELECT * FROM doctors_account WHERE  DoctorCode='$id' ");
+    $row=mysqli_fetch_assoc($Select);
+    // $Doctor_Code = $row['DoctorCode'];
+    $qualifications=$row['qualifications'];
+    $date_of_birth =$row['date_of_birth'];
+    $hiring_date =$row['hiring_date'];  
+    $Doctor_ar_Name = $row['Doctor_ar_Name'];              
+    $Doctor_eng_Name=$row["Doctor_eng_Name"];                   
+    $National_id=$row["National_id"];                   
+    $Mobile=$row["Mobile"];                   
+    $Academic_Mail=$row["Academic_Mail"]; 
+    $Personal_Mail=$row["Personal_Mail"]; 
+    $Notes=$row["Notes"]; 
+    $Doctor_image=$row["Doctor_image"]; 
+    $department=$row["departments"]; 
+    $university=$row["university"];
+    $faculty=$row["faculty"];
+    $doctorjob=$row["doctor_jobs"];}
+
+if (isset($_POST['updateMemberData'])){
+    // if (isset($_POST['Doctor_ar_Name']) && isset($_POST['Doctor_eng_Name']) &&
+    //     isset($_POST['National_id']) && isset($_POST['Mobile']) &&
+    //     isset($_POST['Academic_Mail']) && isset($_POST['Personal_Mail'])&& 
+    //     isset($_POST['Doctor_image']) && isset($_POST['departments']) &&
+    //     isset($_POST['university'])&& isset($_POST['faculty'])&& 
+    //     isset($_POST['doctor_jobs'])&& isset($_POST["Notes"])) {
+     
+    // $Doctor_Code = $_POST['DoctorCode'];
+    $qualifications=$row['qualifications'];
+    $date_of_birth =$row['date_of_birth'];
+    $hiring_date =$row['hiring_date']; 
+    $Doctor_ar_Name = $_POST['Doctor_ar_Name'];              
+    $Doctor_eng_Name=$_POST["Doctor_eng_Name"];                   
+    $National_id=$_POST["National_id"];                   
+    $Mobile=$_POST["Mobile"];                   
+    $Academic_Mail=$_POST["Academic_Mail"]; 
+    $Personal_Mail=$_POST["Personal_Mail"]; 
+    $Notes=$_POST["Notes"]; 
+    $Doctor_image=$_POST["Doctor_image"]; 
+    $department=$_POST["departments"]; 
+    $university=$_POST["university"];
+    $faculty=$_POST["faculty"];
+    $doctorjob=$_POST["doctor_jobs"];}
+    if (isset($_GET['id'])){
+         $id=$_GET['id'];
+    $Details = ("UPDATE doctors_account SET 
+    Doctor_ar_Name='$Doctor_ar_Name',Doctor_eng_Name='$Doctor_eng_Name',
+    National_id='$National_id',Mobile='$Mobile',Academic_Mail='$Academic_Mail',
+    Personal_Mail='$Personal_Mail',Notes='$Notes',Doctor_image='$Doctor_image',
+    departments='$department',university='$university',faculty='$faculty',
+    doctor_jobs='$doctorjob' date_of_birth ='$date_of_birth', qualifications='$qualifications';
+    hiring_date ='$hiring_date'  WHERE DoctorCode='18'");}
+    if($Details){
+        $msg="updated";
+    }
+// else{
+//     $Insert = mysqli_query($bis,"INSERT INTO doctors_account(Doctor_ar_Name, Doctor_eng_Name, National_id, Mobile, Academic_Mail, Personal_Mail,Notes, Doctor_image, departments, university, faculty, doctor_jobs)
+//      values($Doctor_ar_Name, $Doctor_eng_Name, $National_id, $Mobile, $Academic_Mail, $Personal_Mail, $Notes,$Doctor_image, $department, $university, $faculty, $doctorjob)");
+//     if($Insert){
+//         $msg="inserted";
+//     }
+//     else{
+//         $msg="not inserted";
+//     }
+// }
+
+    
+
+
+
+
+
+        //  if (isset($_GET['updateMemberData'])){
+        //     $Select = "SELECT * FROM doctors_account WHERE DoctorCode=$id ";
+        //     $UPDATE = "UPDATE  doctors_account(Doctor_ar_Name, Academic_Mail, Personal_Mail) values(?, ?, ?)";
+        // //  $query=  "UPDATE doctors_account SET Doctor_ar_Name= $Doctor_ar_Name , Personal_Mail=$Personal_Mail , Academic_Mail=$Academic_Mail WHERE DoctorCode= $id ";
+        
+          
+        //     $stmt->execute();
+            
+        //     $stmt->store_result();
+        //     $stmt->fetch();
+        //     $rnum = $stmt->num_rows;
+        //     if ($rnum == 0) {
+        //         $stmt->close();
+        //         $stmt = $bis->prepare($Insert);
+        //         $stmt->bind_param("sss",$Doctor_ar_Name, $Academic_Mail, $Personal_Mail);     
+        //                    if ($stmt->execute()) {
+        //             // header("location:system/addMember.php");;
+        //         }
+        //         else {
+        //             echo $stmt->error;
+        //         }
+        //     }
+        // }}}
+        
+    
+// $bis = new mysqli($hostname_bis, $username_bis, $password_bis, $database_bis);
+// if ($bis->connect_error) {
+//     die('Could not connect to the database.');
+// }
+    
+$query_appata = "SELECT * FROM departments";
+$result = $bis->query($query_appata);
+$appata = mysqli_query ($bis, $query_appata) or die (mysqli_error ($bis));
+$row_appata = mysqli_fetch_assoc ($appata);
+$departments=array($row_appata);
+while($row=mysqli_fetch_assoc($appata)){
+    array_push($departments,$row);
+}
+$_SESSION ['departments']=$departments;
+;
+$doctor_jobs = "SELECT * FROM doctor_jobs";
+$result = $bis->query($doctor_jobs);
+$appata = mysqli_query ($bis, $doctor_jobs) or die (mysqli_error ($bis));
+$row_appata = mysqli_fetch_assoc ($appata);
+$doctor_jobs=array($row_appata);
+while($row=mysqli_fetch_assoc($appata)){
+    array_push($doctor_jobs,$row);
+}
+$_SESSION ['doctor_jobs']=$doctor_jobs;
+
+$universities = "SELECT * FROM universities";
+$result = $bis->query($universities);
+$appata = mysqli_query ($bis, $universities) or die (mysqli_error ($bis));
+$row_appata = mysqli_fetch_assoc ($appata);
+$universities=array($row_appata);
+while($row=mysqli_fetch_assoc($appata)){
+    array_push($universities,$row);
+}
+$_SESSION ['universities']=$universities;
+
+$faculties = "SELECT * FROM faculties";
+$result = $bis->query($faculties);
+$appata = mysqli_query ($bis, $faculties) or die (mysqli_error ($bis));
+$row_appata = mysqli_fetch_assoc ($appata);
+$faculties=array($row_appata);
+while($row=mysqli_fetch_assoc($appata)){
+    array_push($faculties,$row);
+}
+$_SESSION ['faculties']=$faculties;
+        ?>
 <!DOCTYPE html>
 <html lang="ar">
 <head>
@@ -38,46 +206,52 @@
                             <i class="fa-solid fa-plus" style="color: #AAB2BA;"></i>
                         </label>
                     </div>
-                    <input type="file" accept="image/*" id="imageSelectionField" class="d-none" name="Doctor_image">
+                    <input type="file" accept="image/*" id="imageSelectionField" class="d-none" value="<?php if (isset($_GET['id'])) {echo $Doctor_image;}?>" name="Doctor_image">
                 </div>
             </div>
             <div class="row my-2">
                 <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder="الاسم باللغة العربية" 	 name="Doctor_ar_Name">
+                    <input type="text" class="form-control" placeholder="الاسم باللغة العربية" value="<?php if (isset($_GET['id'])) {echo $Doctor_ar_Name;}?>" name="Doctor_ar_Name">
                 </div>
                 <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder="الاسم باللغة الانجليزية"  name="Doctor_eng_Name">
+                    <input type="text" class="form-control" placeholder="الاسم باللغة الانجليزية" value="<?php if (isset($_GET['id'])) {echo $Doctor_eng_Name;}?>" name="Doctor_eng_Name">
                 </div>
                 <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder=" الرقم القومى" id="nationalID" name="National_id">
+                    <input type="text" class="form-control" placeholder=" الرقم القومى" id="nationalID" value="<?php if (isset($_GET['id'])) {echo $National_id;}?>" name="National_id">
                 </div>
             </div>
             <div class="row my-2">
                 <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder="تاريخ الميلاد"  name="Doctor_birthdate" id="birthDate">
+                    <input type="text" class="form-control" placeholder="تاريخ الميلاد" value="<?php if (isset($_GET['id'])) {echo $date_of_birth;}?>" name="date_of_birth" id="birthDate">
                 </div>
                 <div class="col-md-4">
-                    <input type="email" class="form-control" placeholder="الايميل الشخصى" id="personalEmail" name="Personal_Mail">
+                    <input type="email" class="form-control" placeholder="الايميل الشخصى" value="<?php if (isset($_GET['id'])) {echo $Personal_Mail;}?>" id="personalEmail" name="Personal_Mail">
                 </div>
                 <div class="col-md-4">
-                    <input type="tel" class="form-control" placeholder="رقم الهاتف" id="phoneNumber" name="Mobile">
+                    <input type="tel" class="form-control" placeholder="رقم الهاتف" value="<?php if (isset($_GET['id'])) {echo $Mobile;}?>" id="phoneNumber" name="Mobile">
                 </div>
             </div>
             <div class="row my-2">
                 <div class="col-md-4">
                     <select name="university" class="form-select" id="university">
-                        <option selected value=''></option>
+                    <?php foreach($universities as $row){?>
+                        <option selected value='<?php echo $row['uni_eng_name'];?>'> <?php echo $row['uni_ar_name']?></option>
+                        <?php } if (isset($_GET['id'])){?> <option selected value=''> <?php echo $university;} ?></option>?>
                     </select>
                 </div>
                 <div class="col-md-4">
                     <select name="faculty" class="form-select" id="faculty">
-                        <option selected value=''></option>
+                    <?php foreach($faculties as $row){?>
+                        <option selected value='<?php echo $row['Faculty_eng_name']?>'><?php echo $row['Faculty_ar_name']?> </option>
+                        <?php } if (isset($_GET['id'])){?> <option selected value=''> <?php echo $faculty;} ?></option>
                     </select>
                 </div>
                 <div class="col-md-4">
                     <select name="departments" class="form-select" id="department">
                     <option selected value="">القسم</option>
-                    <option value=''></option> 
+                    <?php foreach ($departments as $row){?>
+                        <option value='<?php echo $row['Department_eng_name']?>'><?php echo $row['Department_ar_name']?></option> 
+                        <?php } if (isset($_GET['id'])){?> <option selected value=''> <?php echo $department;} ?></option> 
                     </select>
                 </div>
             </div>
@@ -85,29 +259,31 @@
                 <div class="col-md-4">
                     <select name="doctor_jobs" class="form-select" id="job">
                     <option selected value="">الدرجة الوظيفية الحالية</option>
-                    <option value=''></option>
+                    <?php  foreach($doctor_jobs as $row){?>
+                        <option value='<?php echo $row['Doctor_job_eng_name']?>'><?php echo $row['Doctor_job_ar_name']?></option>
+                        <?php } if (isset($_GET['id'])){?> <option selected value=''> <?php echo $doctorjob;} ?></option>
                     </select>
                 </div>
                 <div class="col-md-4">
-                    <input type="text" class="form-control" placeholder="تاريخ التعيين"  name="Doctor_hiringDate" id="hiringDate">
+                    <input type="text" class="form-control" placeholder="تاريخ التعيين" value="<?php if (isset($_GET['id'])) {echo $hiring_date;}?>" name="hiring_date" id="hiringDate">
                 </div>
                 <div class="col-md-4">
-                    <input type="email" class="form-control" placeholder="الايميل الاكاديمى" id="academicEmail" name="Academic_Mail">
+                    <input type="email" class="form-control" placeholder="الايميل الاكاديمى" value="<?php if (isset($_GET['id'])) {echo $Academic_Mail;}?>" id="academicEmail" name="Academic_Mail">
                 </div>
             </div>
             <div class="row my-2">
                 <div class="col-md-12">
-                    <textarea name="Qualifications" id="Qualifications" rows="3"  placeholder="المؤهلات العلمية" class="form-control fs-4"></textarea>
+                    <textarea name="qualifications" id="Qualifications" rows="3" value="<?php if (isset($_GET['id'])) {echo $qualifications;}?>" placeholder="المؤهلات العلمية" class="form-control fs-4"></textarea>
                 </div>
             </div>
             <div class="row my-2">
                 <div class="col-md-12">
-                    <textarea name="Notes" id="notes" rows="3"  placeholder="ملاحظــــــــــــات" class="form-control fs-4"></textarea>
+                    <textarea name="Notes" id="notes" rows="3"  placeholder="ملاحظــــــــــــات" value="<?php if (isset($_GET['id'])) {echo $Notes;}?>" class="form-control fs-4"></textarea>
                 </div>
             </div>
             <div class="row my-2 justify-content-end">
                 <div class="col-md-2">
-                    <button type="submit" class="updateMemberDataBtn rounded-pill border-0 w-100 my-3"  id="updateMemberData" name="updateMemberData">تعديل</button>
+                <a href="memberDetails.php?id=<?php echo $Doctor_Code?>"><button type="submit" class="updateMemberDataBtn rounded-pill border-0 w-100 my-3"  id="updateMemberData" name="updateMemberData">تعديل</button></a>
                 </div> 
             </div>
         </div>
